9849bf110410a19498ac45961e27ccac4e24d0b3
Damdinsvren2020 <56712755+Damdinsvren2020@users.noreply.github.com>
2022-07-21 18:05:41 +0800

    token

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -11,10 +11,47 @@ import Product from "../product/product";
 import Banner from "../home_banner/Home_banner";
 import Brand from "../brand/brand";
 import Banner_images from "../banner_images/banner_images";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
   const [whichComponent, setWhichComponent] = useState("");
+  const [refresh, setRefresh] = useState(0);
+  const [user, setUser] = useState([]);
+
+  const history = useNavigate();
+
+  const user_token = localStorage.getItem("token");
+  useEffect(() => {
+    if (user_token) {
+      const authorize = async () => {
+        try {
+          const { data } = await axios.get("authorize", {
+            headers: {
+              authorization: "Bearer " + user_token,
+            },
+          });
+          if (data.success) {
+            setUser(data.user);
+            if (data.role === "user") {
+              return history("/home");
+            }
+            if (data.role === "admin") {
+              return history("/home");
+            }
+          } else {
+            history("/Login");
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      };
+      authorize();
+    } else {
+      history("/Login");
+    }
+  }, [history, refresh]);
 
   const RenderComponent = () => {
     switch (whichComponent) {
